51e7c2e31a4ec69c2b9242af3407f01aebe3fdad
emil-marlo <zenlord5.er@gmail.com>
2019-06-05 19:35:37 +0800

    Init Commit - Done Challenge

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+
+// const title = document.querySelector(".newPostTitle").innerHTML;
+// const p = document.querySelector(".newContent");
+// const config = {
+//   character: "...<a href='/post/" + title +'">Read More</a>"
+// }
+// shave(p, 40, config);
